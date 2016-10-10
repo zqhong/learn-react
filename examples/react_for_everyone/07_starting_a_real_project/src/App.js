4f0efe79bdb7280856462df26ac24b8c77963ba7
zqhong <i@zqhong.com>
2016-10-10 22:52:42 +0800

    Exporting & Importing A Child Component

--- examples/react_for_everyone/07_starting_a_real_project/src/App.js
+++ examples/react_for_everyone/07_starting_a_real_project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ContactsList from './ContactsList';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Contacts List</h1>
+                <ContactsList />
             </div>
         )
     }
---
+++ examples/react_for_everyone/07_starting_a_real_project/src/ContactsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class ContactsList extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li>Sky 555 555 555</li>
+            </ul>
+        )
+    }
+}
+
+export default ContactsList;
